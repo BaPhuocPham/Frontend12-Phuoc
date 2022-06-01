1cfed4ffe3a36134173912425cee10f09ad8dbba
BaPhuocPham <phuoc.18112002@gmail.com>
2022-06-01 16:34:00 +0700

    update

--- musicPlayerWeb/js/main.js
+++ musicPlayerWeb/js/main.js
@@ -1,8 +1,18 @@
-$(document).ready(function () {
-  $.get("https://api.apify.com/v2/key-value-stores/EJ3Ppyr2t73Ifit64/records/LATEST?fbclid=IwAR0HkDrTsVyqV6WRZFE7SDwmcU5WHafNStsdAupjHLdFvuPYTvG1khaHjQA", 
-    function (data) {
-      console.log(data)
-    },
-    "JSON"
-  );
-});
+// $(document).ready(function () {
+//   let dataAPI = $.get(
+//     "https://api.apify.com/v2/key-value-stores/EJ3Ppyr2t73Ifit64/records/LATEST?fbclid=IwAR0HkDrTsVyqV6WRZFE7SDwmcU5WHafNStsdAupjHLdFvuPYTvG1khaHjQA", 
+//     function (data) {
+//       console.log(data)
+//     },
+//     "json"
+//   );
+//       console.log(dataAPI)
+// });
+
+
+   
+const api = "https://api.apify.com/v2/key-value-stores/EJ3Ppyr2t73Ifit64/records/LATEST?fbclid=IwAR0HkDrTsVyqV6WRZFE7SDwmcU5WHafNStsdAupjHLdFvuPYTvG1khaHjQA"
+fetch(api)
+      .then(res => res.json())
+      .then(data => console.log(data))
+
